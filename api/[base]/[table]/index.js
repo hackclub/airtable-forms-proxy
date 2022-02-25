b0636b27730dcc3cad3e5a1c5dd22bf3b4aac44b
Gary Tou <gary@garytou.com>
2022-02-24 19:16:36 -0800

    Don't redirect if no url is provided

--- api/[base]/[table]/index.js
+++ api/[base]/[table]/index.js
@@ -12,9 +12,12 @@ export default async function handler(req, res) {
   const returnedByAirtable = await airtable.create(req.body);
   
   try {
-    res.writeHead(302, { Location: req.query.redirect }).end();
-  }
-  catch {
+    if (req.query.redirect) {
+      res.writeHead(302, { Location: req.query.redirect }).end();
+    } else {
+      res.status(204).end();
+    }
+  } catch {
     res.json(returnedByAirtable);
   }
 }
